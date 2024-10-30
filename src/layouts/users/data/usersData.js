3109467171b7de44c13536353417c90b69412e7f
jaofreire <joao.gabriel18872@gmail.com>
2024-10-30 18:33:10 -0300

    feat: funcionalidade para remover usuarios com permissões

--- src/layouts/users/data/usersData.js
+++ src/layouts/users/data/usersData.js
@@ -8,6 +8,7 @@ import PropTypes from "prop-types";
 import Swal from "sweetalert2";
 import MDButton from "components/MDButton";
 import { Icon } from "@mui/material";
+import Cookies from "js-cookie";
 
 const UserCredential = ({ credential }) => (
   <MDBox display="flex" lineHeight={1}>
@@ -36,6 +37,75 @@ export default function usersData() {
       .catch((e) => console.log(e));
   };
 
+  const removeUser = async (id, role) => {
+    const userRole = Cookies.get("user_role");
+    if (validateRoles(role, userRole)) {
+      Swal.fire({
+        text: "Deseja excluir este item?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim",
+      }).then((swalResult) => {
+        if (swalResult.isConfirmed) {
+          api
+            .delete("api/v1/User/" + id)
+            .then((response) => {
+              if (response.data.IS_OPERATION_SUCCESS === true) {
+                Swal.fire({
+                  position: "center",
+                  icon: "success",
+                  title: response.data.MESSAGE,
+                  showConfirmButton: false,
+                  timer: 1500,
+                }).then(() => {
+                  window.location.reload();
+                });
+              } else {
+                Swal.fire({
+                  position: "center",
+                  icon: "error",
+                  title: response.data.MESSAGE,
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+              }
+            })
+            .catch((error) => {
+              console.error(error);
+              console.error(error.data);
+            });
+        }
+      });
+    }
+  };
+
+  const validateRoles = (role, userRole) => {
+    if (role === "ADMIN" && userRole === "ADMIN") {
+      Swal.fire({
+        text: "Não é possível remover um ADMIN sendo um ADMIN",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Ok",
+      });
+      return false;
+    }
+    if (role === "SP_ADMIN" && userRole === "ADMIN") {
+      Swal.fire({
+        text: "Não é possível remover um SuperAdmin sendo um ADMIN",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Ok",
+      });
+      return false;
+    }
+
+    return true;
+  };
+
   return {
     columns: [
       { Header: "Nome de usuário", accessor: "user_name", width: "20%", align: "left" },
@@ -43,6 +113,7 @@ export default function usersData() {
       { Header: "Senha", accessor: "password", width: "20%", align: "left" },
       { Header: "Créditos", accessor: "credits", width: "20%", align: "left" },
       { Header: "Tipo", accessor: "user_type", width: "20%", align: "left" },
+      { Header: "Remover", accessor: "remove_button", width: "45%", align: "left" },
     ],
     rows: users.map((user) => ({
       user_name: <UserCredential credential={user.userName} />,
@@ -50,6 +121,11 @@ export default function usersData() {
       password: <UserCredential credential={user.password} />,
       credits: <UserCredential credential={user.credits} />,
       user_type: <UserCredential credential={user.role} />,
+      remove_button: (
+        <MDButton variant="outlined" color="error" onClick={() => removeUser(user.id, user.role)}>
+          <Icon>delete</Icon>
+        </MDButton>
+      ),
     })),
   };
 }
